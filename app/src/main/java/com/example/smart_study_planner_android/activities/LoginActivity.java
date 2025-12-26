c5c63b0b52c3a5d3193a0a2f437322d13758c414
AnweshaDS <anwesha.das.bd.03@gmail.com>
2025-12-27 01:25:34 +0600

    first page is working

--- app/src/main/java/com/example/smart_study_planner_android/activities/LoginActivity.java
+++ app/src/main/java/com/example/smart_study_planner_android/activities/LoginActivity.java
@@ -1,4 +1,25 @@
 package com.example.smart_study_planner_android.activities;
 
-public class LoginActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.smart_study_planner_android.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(v -> {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        });
+    }
 }
